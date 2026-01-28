81d58eb075dcfeb8573d10df9f76c50f5e307a6b
nemeskalito <borisenko.igor2021@yandex.ru>
2026-01-28 22:49:34 +0300

    Чатбот

---
+++ chatBot.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+require('dotenv').config();
+const { initAntiLinks } = require('./modules/antiLinks.js');
+const { initGreeting } = require('./modules/greeting.js');
+
+const bot = new TelegramBot(process.env.API_CHATBOT, { polling: true });
+
+initAntiLinks(bot);
+initGreeting(bot);
